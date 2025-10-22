04f544981c3db9637a729b405a8e573dcc7f99d7
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-21 18:12:29 -0700

    add bob example

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
 import { useState, useEffect } from 'react'
-import supabase from '../utils/supabase'
+import supabase from './utils/supabase'
 
 function Page() {
-  const [todos, setTodos] = useState([])
+  const [employees, setEmployees] = useState([])
 
   useEffect(() => {
-    async function getTodos() {
-      const { data: todos } = await supabase.from('todos').select()
+    async function getEmployees() {
+      const { data: employees } = await supabase.from('employees').select()
 
-      if (todos.length > 1) {
-        setTodos(todos)
+      if (employees.length > 1) {
+        setEmployees(employees)
       }
     }
 
-    getTodos()
+    getEmployees()
   }, [])
+
+  async function addEmployee(firstName, lastName, favIceCream, shift) {
+    const newEmployee = {
+      first_name: "Bob",
+      last_name: "Dole",
+      fav_ice_cream: "Rocky Road",
+      shift: "morning",
+      date_of_hire: "2025-10-21"
+    }
+    const { data } = await supabase.from('employees').insert(newEmployee)
+  }
+
+  function handleAddBob() {
+    addEmployee();
+  }
+
+
 
   return (
     <div>
-      {todos.map((todo) => (
-        <li key={todo}>{todo}</li>
-      ))}
+      <ul>
+        {employees.map((employee) => (
+          <li key={employee.id}>
+            {employee.first_name} | Favorite Ice Cream: {" "}
+            {employee.fav_ice_cream}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <button onClick={handleAddBob}>Add Bob</button>
+      </div>
     </div>
   )
 }
--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,6 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+// console.log(supabaseUrl)
+// console.log(supabaseKey)
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
