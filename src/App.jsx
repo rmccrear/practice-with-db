d569a9b1c7c891b7572c30f29992cd9eef1059cd
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-21 18:36:29 -0700

    add form and insert into database

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,33 @@ function Page() {
     addEmployee();
   }
 
+  async function handleSubmit(event) {
+    event.preventDefault()
+    console.log("in handle submit")
+    console.log(event)
+    const firstName = event.target.elements.firstName.value
+    const lastName = event.target.elements.lastName.value
+    const favIceCream = event.target.elements.favIceCream.value
+    console.log(firstName, lastName, favIceCream)
+
+    const newEmployee = {
+      first_name: firstName,
+      last_name: lastName,
+      fav_ice_cream: favIceCream
+    }
+    console.log(newEmployee)
+    await supabase.from("employees").insert(newEmployee)
+
+    // get fresh data
+    const response = await supabase.from("employees").select()
+    const data = response.data
+    console.log(data)
+    setEmployees(data)
+    
+    event.target.elements.firstName.value = ""
+    event.target.elements.lastName.value = ""
+    event.target.elements.favIceCream.value = "" 
+  }
 
 
   return (
@@ -44,7 +71,29 @@ function Page() {
         ))}
       </ul>
       <div>
-        <button onClick={handleAddBob}>Add Bob</button>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              First Name:
+              <input type="text" name="firstName" id="firstName" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Last Name:
+              <input type="text" name="lastName" id="lastName" />
+            </label>
+          </div>
+          <div>
+            <label>
+              Favorite Ice Cream:
+              <input type="text" name="favIceCream" id="favIceCream" />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Add Employee</button>
+          </div>
+        </form>
       </div>
     </div>
   )
