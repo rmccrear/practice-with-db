e0e1ff6197520cdfbaa31521b6502892cfbb838e
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-11-06 19:26:08 -0800

    add LoginPage Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { useState } from 'react'
 import PotluckMeals from './components/PotluckMeals'
+import LoginPage from './LoginPage'
 import DUMMY_USER from "./data/user.json"
 
 
-let user = DUMMY_USER
-console.log(user)
+// let user = DUMMY_USER
+// console.log(user)
 // set undefined for guest
 // user=undefined;
 
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  function loginComplete(user, error){
+    if(!error){
+      setUser(user)
+    } else {
+      alert(error)
+    }
+  }
+
   return (<>
-    <h1>Welcome {user ? user.name : "Guest"}</h1>
+    <h1>Welcome {user ? user.email : "Guest"}</h1>
+    {!user && <LoginPage loginComplete={loginComplete}/>}
     <PotluckMeals user={user}/>
   </>)
 }
--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -13,6 +13,7 @@ export default function LoginPage({ loginComplete, cancel }) {
         console.log("handled")
         console.log(error)
         console.log(data)
+        return {user: data.user, error};
     }
 
     async function handleLogin(event) {
@@ -20,19 +21,20 @@ export default function LoginPage({ loginComplete, cancel }) {
         const email = event.target.elements.email.value
         const password = event.target.elements.password.value
         console.log("handle login")
-        await signIn(email, password)
-        loginComplete();
+        const {user, error} = await signIn(email, password)
+        loginComplete(user, error);
     }
 
     return (<>
 
         <form onSubmit={handleLogin}>
             <label>
-                email
+                email: 
                 <input type="text" name="email" />
             </label>
+            <br />
             <label>
-                password
+                password: 
                 <input type="password" name="password" />
             </label>
             <button type="submit">Login</button>
